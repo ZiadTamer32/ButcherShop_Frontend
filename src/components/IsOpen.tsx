566d33bce3bf662bdbb74f1b757415eab5155e24
Ziad Tamer <ziadtamer0543@gmail.com>
2025-11-22 22:22:36 +0200

    Fix Imports

--- src/components/IsOpen.tsx
+++ src/components/IsOpen.tsx
@@ -3,20 +3,19 @@ import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 const IsOpen = ({ children }: { children: React.ReactNode }) => {
-  // const [time, setTime] = useState<number | null>(null);
-  // const navigate = useNavigate();
+  const [time, setTime] = useState<number | null>(null);
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const currentHour = new Date().getHours();
-  //   setTime(currentHour);
-  //   if (currentHour < 7 || currentHour > 23) {
-  //     navigate("/");
-  //     toast.error("المحل مغلق حالياً. ساعات العمل من 7 صباحاً حتى 11 مساءً.");
-  //   }
-  // }, [navigate]);
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    setTime(currentHour);
+    if (currentHour < 7 || currentHour > 5) {
+      navigate("/");
+      toast.error("المحل مغلق حالياً. ساعات العمل من 7 صباحاً حتى 11 مساءً.");
+    }
+  }, [navigate]);
 
-  // const isOpen = time !== null && time >= 7 && time <= 23;
-  const isOpen = true;
+  const isOpen = time !== null && time >= 7 && time <= 5;
   return isOpen ? <>{children}</> : null;
 };
 
